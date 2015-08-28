b4a0ed8c09204a3901c8eabc66bebc467e7e2b2e
Eduardo Trujillo <ed@chromabits.com>
2015-08-28 00:17:14 -0400

    Add phpstd (experimental)

---
+++ Formula/phpstd.rb
@@ -0,0 +1,23 @@
+require 'formula'
+require File.expand_path("../../Requirements/php-meta-requirement", __FILE__)
+require File.expand_path("../../Requirements/phar-requirement", __FILE__)
+
+class Phpstd < Formula
+  homepage 'https://github.com/etcinit/php-coding-standard'
+  url 'https://github.com/etcinit/php-coding-standard/releases/download/v0.1.8/phpstd.phar'
+  sha256 '4bafea623467f233fd562c662824d5ea7d5915cfe5e289b998f61523f504e1fd'
+
+  depends_on PhpMetaRequirement
+  depends_on PharRequirement
+
+  def install
+    libexec.install "phpstd.phar"
+    sh = libexec + "phpstd"
+    sh.write("#!/bin/sh\n\n/usr/bin/env php -d allow_url_fopen=On -d detect_unicode=Off #{libexec}/phpstd.phar $*")
+    chmod 0755, sh
+    bin.install_symlink sh
+  end
+
+  test do
+    system 'phpstd --version'
+  end
---
+++ Requirements/homebrew-php-requirement.rb
@@ -0,0 +1,15 @@
+require File.join(File.dirname(__FILE__), 'homebrew-php-requirement')
+
+class HomebrewPhpRequirement < Requirement
+  def fatal?
+    true
+  end
+
+  def satisfied?
+    false
+  end
+
+  def message
+    "A requirement as failed"
+  end
+end
